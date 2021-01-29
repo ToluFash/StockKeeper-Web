58817301d2587bb3bef24f70406d6ed825508025
ToluFash <faithbullock4@gmail.com>
2021-01-29 14:45:58 +0100

    Ave Maria Multipurpose Investment Init Web

---
+++ Views/Interfaces.js
@@ -0,0 +1,31 @@
+
+class MouseListener {
+
+
+    mouseClicked(e){
+
+    }
+    mouseEntered(e){
+    }
+    mouseLeave(e){
+
+    }
+    mouseMoved(e){
+
+    }
+    mouseOut(e){
+
+    }
+    mouseOver(e){
+
+
+    }
+    mouseDown(e){
+
+    }
+}
+class ComponentListener {
+
+    componentResized(e){
+    }
+}
